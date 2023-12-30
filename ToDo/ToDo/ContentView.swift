c2cdb100f89340c420e5ebeadfa4ac9ec2a90853
Gourob Mazumder <gourobmazumder507@gmail.com>
2023-12-30 15:11:44 +0600

    added tab view

--- ToDo/ToDo/ContentView.swift
+++ ToDo/ToDo/ContentView.swift
@@ -9,10 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isActive = false
-    
+    @State var selectedTab: Tab = .pending
     var body: some View {
         if isActive {
-            Text("Hello")
+            VStack {
+                ViewThatFits {
+                    TabView(selectedTab: $selectedTab)
+                    ScrollView(.horizontal){
+                        TabView(selectedTab: $selectedTab)
+                    }
+                    .scrollIndicators(.hidden)
+                }
+                .background {
+                    Color.accentColor.opacity(0.2)
+                        .ignoresSafeArea(edges:[.top, .horizontal])
+                }
+                switch selectedTab {
+                case .pending:
+                    Text("Pending items")
+                case .completed:
+                    Text("Completed items")
+                case .overdue:
+                    Text("Overdure items")
+                }
+                Spacer()
+            }
         } else {
             SplashScreen()
                 .onAppear {
---
+++ ToDo/ToDo/Tab.swift
@@ -0,0 +1,14 @@
+//
+//  Tab.swift
+//  ToDo
+//
+//  Created by Gourob Mazumder on 30/12/23.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable {
+    case pending = "Pending"
+    case completed = "Completed"
+    case overdue = "Overdue"
+}
