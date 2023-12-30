063130ca336682960d420a9143221f9d232b44ee
Gourob Mazumder <gourobmazumder507@gmail.com>
2023-12-30 13:28:13 +0600

    initiated the project

---
+++ ToDo/ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Gourob Mazumder on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
